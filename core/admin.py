0c49d6be907519d53a33f8659611a4e0ec3ac233
amljamal <amljamaal17@gmail.com>
2025-10-07 12:03:18 +0300

    Refactor models: Custom User, Car, and Booking and update settings

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import  Car, Booking
+from .models import  Car, Booking, User
 
-
+admin.site.register(User)
 admin.site.register(Car)
 admin.site.register(Booking)
